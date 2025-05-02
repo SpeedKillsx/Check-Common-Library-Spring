d4e00e94c81a837d3e5691cf45a40c491de00617
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-05-02 23:14:14 +0200

    Initial Commit

---
+++ src/main/java/com/amayaslab/check_common_lib/CheckCommonLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.amayaslab.check_common_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckCommonLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckCommonLibraryApplication.class, args);
+	}
+
+}
